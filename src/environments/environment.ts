a0991e7eb8e805052349ec965999499f173801da
Arun Sharma <arunsharma060@gmail.com>
2023-07-17 21:55:28 +0530

    implemented boiler plate code

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'AssetArray',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44337/',
+    redirectUri: baseUrl,
+    clientId: 'AssetArray_App',
+    responseType: 'code',
+    scope: 'offline_access AssetArray',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44337',
+      rootNamespace: 'AssetArray',
+    },
+  },
+} as Environment;
